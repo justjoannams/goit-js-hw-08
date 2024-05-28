73b329a1d32b3573242f2601c47f4045657ffa5f
justjoannams <justjoannams@gmail.com>
2024-05-29 02:47:55 +0800

    goit-js-hw-08-simplelightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,7 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import "../css/common.css";
-import "../css/01-gallery.css";
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const createItemsMarkup = galleryItems
   .map(({ preview, original, description }) => {
